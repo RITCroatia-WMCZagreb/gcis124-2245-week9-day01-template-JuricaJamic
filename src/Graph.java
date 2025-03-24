037152d27ff651b3ad7487e647a8ac5f0b95f34e
Jurica Jamic (RIT Student) <jj2262@rit.edu>
2025-03-24 12:45:28 +0100

    Push

---
+++ src/Graph.java
@@ -0,0 +1,19 @@
+/*
+@ASSESSME.USERID: jurica.jamic@rit.edu
+@ASSESSME.AUTHOR: Jurica Jamic
+@ASSESSME.DESCRIPTION: 
+@ASSESSME.ANALYZE: YES
+*/
+
+
+public interface Graph<E>{
+    void add(E value);
+    boolean contains(E value);
+    int size();
+    void connectDirected(E a, E b);
+    void connectUndirected(E a, E b);
+    boolean connected(E a, E b);    
+}
+
+
+
---
+++ src/Vertex.java
@@ -0,0 +1,49 @@
+/*
+@ASSESSME.USERID: jurica.jamic@rit.edu
+@ASSESSME.AUTHOR: Jurica Jamic
+@ASSESSME.DESCRIPTION: 
+@ASSESSME.ANALYZE: YES
+*/
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Vertex<E> {
+    private E value;
+    private Set<Vertex<E>> neighbours = null;
+
+    public Vertex(E value){
+        this.value = value;
+        this.neighbours = new HashSet<>();
+    }
+
+    public E getValue(){
+        return this.value;
+    }
+
+    public void connect(Vertex<E> neighbour){
+        this.neighbours.add(neighbour);
+    }
+
+    public boolean connected(Vertex<E> neighbour){
+        return this.neighbours.contains(neighbour);
+    }
+
+    public Set<Vertex<E>> getneighbours(){
+        return this.neighbours;
+    }
+
+    public static void main(String[] args) {
+        Vertex<String> vA = new Vertex<String>("A");
+        Vertex<String> vB = new Vertex<String>("B");
+        Vertex<String> vC = new Vertex<String>("C");
+
+        vA.connect(vB);
+        vA.connect(vC);
+        vB.connect(vC);
+        vC.connect(vA);
+        vC.connect(vB);
+    }
+
+
+}
